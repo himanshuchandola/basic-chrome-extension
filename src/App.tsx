eab93d5dc95f65dc82864efba363ceb4e2181759
himanshuchandola <96554303+himanshuchandola@users.noreply.github.com>
2024-01-30 20:04:17 +0530

    few changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Himanshu Chandola</div>;
+  return (
+    <div className="App">
+      <h1>Himanshu Chandola</h1>
+      <div className="github-link">
+        <a
+          href="https://github.com/himanshuchandola/basic-chrome-extension"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Extension Github Repo
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
